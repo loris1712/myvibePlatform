5bccc9b1662baab47aae92083090f5d6d1d59cc7
Loris Caputo <lorixyes@gmail.com>
2023-10-12 22:24:32 +0200

    Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,7 +69,7 @@ function Header() {
 
       <nav className={`navbar navbar-expand-lg fixed-top mobile ${isActive ? 'active_background' : ''}`}>
         <div className="container-fluid">
-            <a className="navbar-brand navbar-button" href="#">Explore</a>
+            <a className="navbar-brand navbar-button" href="/explore?city=New York">Explore</a>
             <a className="navbar-brand" href="/">myvibe</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="svg-icon">
